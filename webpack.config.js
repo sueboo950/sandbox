80c23ba1c719d229033f7eca1395e83091b86b98
sophie950 <sueboo950950@gmail.com>
2025-03-18 19:11:40 -0500

    completed project with criteria

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // Ensures the 'dist' folder is cleaned
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'], // For modern JavaScript support
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader', // Add Autoprefixer via PostCSS
+                    'sass-loader', // Compiles SCSS to CSS
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'), // Serves files from 'dist'
+        },
+        compress: true,
+        hot: true,
+        port: 9000,
+    },
+};
